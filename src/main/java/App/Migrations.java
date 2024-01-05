1e774138b593e0f513c80916251a8b25233b78d0
4rcan31 <da.esfra12@gmail.com>
2024-01-05 12:44:42 -0600

    add migrations example system

---
+++ src/main/java/App/Migrations.java
@@ -0,0 +1,27 @@
+package App;
+
+import java.util.Arrays;
+
+import core.db.migration.Schema;
+import core.db.migration.Table;
+
+public class Migrations {
+
+    private Schema schema = new Schema("dbtest");
+
+    public void userTable() {
+        Table users = schema.addTable("user");
+        users.addColumn("id", users.id());
+        users.addColumn("name", Arrays.asList("string", "notnull"));
+        users.addColumn("email", Arrays.asList("string", "notnull"));
+        users.addColumn("password", Arrays.asList("string", "notnull"));
+        users.addColumn("update_at", users.updateAt());
+        users.addColumn("create_at", users.createAt());
+    }
+
+    public static void main(String[] args) {
+        Migrations migrations = new Migrations();
+        migrations.userTable();
+        migrations.schema.save();
+    }
+}
